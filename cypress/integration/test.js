221ac4f121909f193bfe4cf9e1521ca75a601b87
hyfa@expentor.com <hyfa@expentor.com>
2021-12-01 16:35:23 +0530

    gitsetup

---
+++ cypress/integration/test.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('frontpage',() =>{
+
+    it('Load Website',()=>{
+    
+        cy.visit(" https://wesbanco-dev.insuranceaisle.com");
+    
+        cy.wait(10000);
+    
+       
+    
+       
+    
+    })
+})
